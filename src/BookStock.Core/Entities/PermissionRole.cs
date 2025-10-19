28d48d68cb384cbaf6b2221cdbf3af4afdafed02
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:13:39 +0500

    Permission Role Model

---
+++ src/BookStock.Core/Entities/PermissionRole.cs
@@ -0,0 +1,9 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class PermissionRole : BaseEntity
+{
+    public int PermissionId { get; set; }
+    public int RoleId { get; set; }
+}
