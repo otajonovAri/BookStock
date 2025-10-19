a1d738ad15648c64e2b9c328a424625c611c3842
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:14:45 +0500

    Tags Model

---
+++ src/BookStock.Core/Entities/Tag.cs
@@ -0,0 +1,12 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class Tag : BaseEntity
+{
+    public int EntityId { get; set; }
+    public string? EntityType { get; set; }
+    public string? Name { get; set; }
+    public string? Value { get; set; }
+    public int Order { get; set; }
+}
