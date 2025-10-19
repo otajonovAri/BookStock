4caaebd92088322176165fb6f256f17b29f8bc57
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 08:52:59 +0500

    View Model

---
+++ src/BookStock.Core/Entities/View.cs
@@ -0,0 +1,11 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class View : BaseEntity
+{
+    public int UserId { get; set; }
+    public int ViewableId { get; set; }
+    public string? ViewableType { get; set; }
+    public int Views { get; set; }
+}
