6ab7d66f00452a636af698d79735be7503cb3dd2
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:14:20 +0500

    Session Model

---
+++ src/BookStock.Core/Entities/Session.cs
@@ -0,0 +1,12 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class Session : BaseEntity
+{
+    public int UserId { get; set; }
+    public string? IpAddress { get; set; }
+    public string? UserAgent { get; set; }
+    public string? Payload { get; set; }
+    public int LastActivity { get; set; }
+}
