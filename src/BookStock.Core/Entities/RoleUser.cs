7c6a6672be2b8210bdc6ec13750e429cda9b2b32
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:14:03 +0500

    Role User

---
+++ src/BookStock.Core/Entities/RoleUser.cs
@@ -0,0 +1,9 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class RoleUser : BaseEntity
+{
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
+}
