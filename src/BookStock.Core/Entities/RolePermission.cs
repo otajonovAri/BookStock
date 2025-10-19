b011b949189a44d854168c4a4faa403755aadcf0
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:13:55 +0500

    Role Permission

---
+++ src/BookStock.Core/Entities/RolePermission.cs
@@ -0,0 +1,10 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class RolePermission : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public string? DisplayName { get; set; }
+    public string? Description { get; set; }
+}
