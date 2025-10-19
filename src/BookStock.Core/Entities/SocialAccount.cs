941c0e84d9c549194ecdc76ea7bb7c498ae16bbc
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:14:38 +0500

    Social Accounts

---
+++ src/BookStock.Core/Entities/SocialAccount.cs
@@ -0,0 +1,11 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class SocialAccount : BaseEntity
+{
+    public int UserId { get; set; }
+    public string? Driver { get; set; }
+    public int DriverId { get; set; }
+    public string? Avatar { get; set; }
+}
