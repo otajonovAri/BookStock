d1220cccb0fe34adc521f164a99d0a9aff53a86e
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:14:12 +0500

    SearchTerm

---
+++ src/BookStock.Core/Entities/SearchTerm.cs
@@ -0,0 +1,11 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class SearchTerm : BaseEntity
+{
+    public string? Term { get; set; }
+    public string? EntityType { get; set; }
+    public int EntityId { get; set; }
+    public int Score { get; set; }
+}
