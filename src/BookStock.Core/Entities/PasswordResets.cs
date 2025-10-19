0ce71df94428faa7071f011d44a9984cb800e6f4
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:13:26 +0500

    Reset Password

---
+++ src/BookStock.Core/Entities/PasswordResets.cs
@@ -0,0 +1,10 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class PasswordResets : BaseEntity
+{
+    public string Email { get; set; } = null!;
+    public string Token { get; set; } = null!;
+    public DateTime CreateAt { get; set; }
+}
