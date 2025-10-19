ec8180eea441cbd7c4b0b96b1fff006a1164731a
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:12:52 +0500

    Page Model

---
+++ src/BookStock.Core/Entities/Page.cs
@@ -0,0 +1,13 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class Page : BaseEntity
+{
+    public int BookId { get; set; }
+    public int ChapterId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Slug { get; set; } = null!;
+    public string Html { get; set; } = null!;
+    public string? Text { get; set; } 
+}
