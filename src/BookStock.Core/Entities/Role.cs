a9b1cb657aabab6e91eafc98ac32e64fd70034e4
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:13:46 +0500

    Role Model

---
+++ src/BookStock.Core/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class Role : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public string? DisplayName { get; set; }
+    public string? Description { get; set; }
+    public string? SystemName { get; set; }
+    public string ExternalAuthId { get; set; } = null!;
+}
