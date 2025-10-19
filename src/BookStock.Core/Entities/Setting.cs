ddeaab5c14c9d06a87a285b4989a574c29145a60
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:14:28 +0500

    Setting Model

---
+++ src/BookStock.Core/Entities/Setting.cs
@@ -0,0 +1,9 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class Setting : BaseEntity
+{
+    public string SettingKey { get; set; } = null!;
+    public string? Value { get; set; }
+}
