4030125533863656612e53490dd67067242cea87
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-10-19 09:13:13 +0500

    User Model

---
+++ src/BookStock.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class User : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string RememberToken { get; set; } = null!;
+    public int EmailConfirmed { get; set; }
+    public int ImgId { get; set; }
+    public int ExternalAuthId { get; set; }
+    public string? SystemName { get; set; }
+}
---
+++ src/BookStock.Core/Entities/UserInvites.cs
@@ -0,0 +1,9 @@
+﻿using BookStock.Core.Common;
+
+namespace BookStock.Core.Entities;
+
+public class UserInvites : BaseEntity
+{
+    public int UserId { get; set; }
+    public string Token { get; set; } = null!;
+}
